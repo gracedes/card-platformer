eb42c94b72c4bc37e8b1e658ed53047ea727d3aa
Grace <gracedes@bu.edu>
2023-10-19 11:33:46 -0400

    animated jump

--- assets/scripts/player/playerMovement.gd
+++ assets/scripts/player/playerMovement.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var _animated_sprite = $AnimatedSprite2D
 
 @export var SPEED = 150.0
-@export var JUMP_VELOCITY = -400.0
+@export var JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
