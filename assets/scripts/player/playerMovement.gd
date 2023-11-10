4394826e7534a37cf3ea0f181cf83b7c7db6f79a
Grace <gracedes@bu.edu>
2023-11-10 13:23:36 -0500

    added card display for fire ability

--- assets/scripts/player/playerMovement.gd
+++ assets/scripts/player/playerMovement.gd
@@ -26,7 +26,7 @@ var num_cloud = 3
 var num_wave = 3
 var num_plant = 3
 var num_fire = 3
-var num_sling = 15
+var num_sling = 3
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
