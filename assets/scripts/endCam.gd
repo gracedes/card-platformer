1e803c28624200452dbb9128e82b98a0dffae9eb
Grace <gracedes@bu.edu>
2023-12-02 04:45:24 -0500

    updated end screen

--- assets/scripts/endCam.gd
+++ assets/scripts/endCam.gd
@@ -1,6 +1,7 @@
 extends Camera2D
 
 @onready var alt = get_node("../Node2D")
+@export var next = "res://scenes/title.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,3 +12,6 @@ func _ready():
 func _process(delta):
 	if alt.timeSinceIgnite > 3.0 and position.y > -412.0:
 		position.y -= delta * 20.0
+	elif alt.timeSinceIgnite > 3.0:
+		if Input.is_action_just_pressed("left-click"):
+			get_tree().change_scene_to_file(next)
